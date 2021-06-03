0d128b0a683ee845ca3085a24a91b6745a92567f
zearkiatos <caprilespe@outlook.com>
2021-06-02 21:44:03 -0400

    Dockerization project for development and deploy, and add github actions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 if [ $(echo "$NODE_ENV")=="develop" ]
 then
-    npm run dev
+    unset MOVIES_API_BASE_URL
+    export MOVIES_API_BASE_URL=http://localhost:8080/
+    npm install -g @angular/cli
+    ng serve --host=0.0.0.0 --port=8000 --live-reload
 else
     npm run build
     npm run start
